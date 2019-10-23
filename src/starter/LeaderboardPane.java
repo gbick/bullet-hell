4bca000bb9217b7dc7c63f3a686b0e461ee7ac2d
gbick <gabrielbick1@gmail.com>
2019-10-23 15:00:34 -0700

    Created new LeaderboardPane class to show leaderboard screen

---
+++ src/starter/LeaderboardPane.java
@@ -0,0 +1,37 @@
+package starter;
+
+import java.awt.event.MouseEvent;
+
+import acm.graphics.GImage;
+import acm.graphics.GObject;
+
+public class LeaderboardPane extends GraphicsPane {
+	private MainApplication program; // you will use program to get access to
+										// all of the GraphicsProgram calls
+	//TODO Identify required objects here
+	
+	//=====
+
+	public LeaderboardPane(MainApplication app) {
+		this.program = app;
+		//TODO Declare object properties here
+		
+		//=====
+	}
+
+	@Override
+	public void showContents() {
+		//TODO program.add(" ") all objects that should be immediately visible on load
+		
+		//=====
+	}
+
+	@Override
+	public void hideContents() {
+		//TODO program.remove(" ") all objects
+		
+		//=====
+	}
+
+	//TODO Add all mouse/key events below here \/ \/ \/ 
+}
