c8868cd8ca8d1e8d4b74c2bb14c665cff81e30aa
ianh <ian.higa@sbcglobal.net>
2019-10-23 14:56:07 -0700

    Changed SomePane to a sample of how to build a new pane for future additions

--- src/starter/SomePane.java
+++ src/starter/SomePane.java
@@ -7,34 +7,30 @@ import acm.graphics.GObject;
 public class SomePane extends GraphicsPane {
 	private MainApplication program; // you will use program to get access to
 										// all of the GraphicsProgram calls
-	private GImage img;
-	private GParagraph para;
+	//TODO Identify required objects here
+	
+	//=====
 
 	public SomePane(MainApplication app) {
 		this.program = app;
-		img = new GImage("robot head.jpg", 100, 100);
-		para = new GParagraph("welcome\nto my\nsecret room!", 150, 300);
-		para.setFont("Arial-24");
+		//TODO Declare object properties here
+		
+		//=====
 	}
 
 	@Override
 	public void showContents() {
-		program.add(img);
-		program.add(para);
+		//TODO program.add(" ") all objects that should be immediately visible on load
+		
+		//=====
 	}
 
 	@Override
 	public void hideContents() {
-		program.remove(img);
-		program.remove(para);
+		//TODO program.remove(" ") all objects
+		
+		//=====
 	}
 
-	@Override
-	public void mousePressed(MouseEvent e) {
-		para.setText("you need\nto click\non the eyes\nto go back");
-		GObject obj = program.getElementAt(e.getX(), e.getY());
-		if (obj == img) {
-			program.switchToMenu();
-		}
-	}
+	//TODO Add all mouse/key events below here \/ \/ \/ 
 }
