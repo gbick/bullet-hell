8c77d834a08dd51fbbfc114af99b8c760e80f7f7
gbick <gabrielbick1@gmail.com>
2019-10-24 15:54:20 -0700

    Added nextLevel and prevLevel GButtons

--- src/starter/LeaderboardPane.java
+++ src/starter/LeaderboardPane.java
@@ -1,21 +1,36 @@
 package starter;
 
 import java.awt.event.MouseEvent;
+import java.util.HashMap;
 
 import acm.graphics.GImage;
+import acm.graphics.GLabel;
 import acm.graphics.GObject;
+import javafx.util.Pair;
 
 public class LeaderboardPane extends GraphicsPane {
 	private MainApplication program; // you will use program to get access to
 										// all of the GraphicsProgram calls
 	//TODO Identify required objects here
+	private GLabel level;
+	private int levelNumber;
+	private GLabel leaderboard;
+	private GLabel easy;
+	private GLabel medium;
+	private GLabel hard;
+	private GButton nextLevel;
+	private GButton prevLevel;
+	private static final int LEADERBOARD_X = MainApplication.WINDOW_HEIGHT/5;
+	private HashMap<Integer, Pair<String, Integer>> scores; // first element is a 3-digit int - 
 	
 	//=====
 
 	public LeaderboardPane(MainApplication app) {
 		this.program = app;
 		//TODO Declare object properties here
-		
+		leaderboard = new GLabel("Leaderboard");
+		nextLevel = new GButton("Next Level", 800, 0, 200, 200);
+		prevLevel = new GButton("Previous Level", 0, 0, 200, 200);
 		//=====
 	}
 
@@ -34,4 +49,9 @@ public class LeaderboardPane extends GraphicsPane {
 	}
 
 	//TODO Add all mouse/key events below here \/ \/ \/ 
+	
+	public void mousePressed(MouseEvent e) {
+		GObject obj = program.getElementAt(e.getX(), e.getY());
+		if (obj == )
+	}
 }
