dab7ced75eafc791721d7ae90ffcdd8eb957f07b
Dereck Vicevich <vice00der10@gmail.com>
2019-10-30 15:56:08 -0700

    Updated the controls to use GButtons instead of GLabels so it can fit the pop up screen size

--- src/starter/ControlsPop.java
+++ src/starter/ControlsPop.java
@@ -12,17 +12,20 @@ public class ControlsPop extends GraphicsPane {
 	private MainApplication program;
 	
 	GButton frame;
-	GLabel label1;
-	GLabel label2;
-	GLabel label3;
+	GButton label1;
+	GButton label2;
+	GButton label3;
 	
 	public ControlsPop(MainApplication app)
 	{
 		this.program = app;
 		frame = new GButton("", program.getWidth()/4, program.getHeight()/4, program.getWidth()/2, program.getHeight()/2);
-		label1 = new GLabel("Mouse - Move Ship", frame.getX()  + frame.getWidth()/65, frame.getY());
-		label2 = new GLabel("Shoot - Left Click", frame.getX() + frame.getWidth()/65, frame.getY() + frame.getHeight()/3 + frame.getWidth()/65);
-		label3 = new GLabel("Space - Super Shot", frame.getX() + frame.getWidth()/65, frame.getY() + ((frame.getHeight()/3) * 2) + frame.getWidth()/65);
+		label1 = new GButton("Mouse - Move Ship", frame.getX()  + frame.getWidth()/65, frame.getY()  + frame.getWidth()/65,
+				frame.getWidth() - ((frame.getWidth()/65) * 2), frame.getHeight()/3 - ((frame.getWidth()/65) * 2));
+		label2 = new GButton("Shoot - Left Click", frame.getX() + frame.getWidth()/65, frame.getY() + frame.getHeight()/3 + frame.getWidth()/65,
+				frame.getWidth() - ((frame.getWidth()/65) * 2), frame.getHeight()/3 - ((frame.getWidth()/65) * 2));
+		label3 = new GButton("Space - Super Shot", frame.getX() + frame.getWidth()/65, frame.getY() + ((frame.getHeight()/3) * 2) + frame.getWidth()/65,
+				frame.getWidth() - ((frame.getWidth()/65) * 2), frame.getHeight()/3 - ((frame.getWidth()/65) * 2));
 	}
 
 	@Override
