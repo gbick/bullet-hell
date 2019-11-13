23016c8afe2dea5bf999be31f9adee0c7c9d687e
ianh <ian.higa@sbcglobal.net>
2019-11-12 18:44:41 -0800

    Started creation of level reading file

---
+++ src/starter/LevelReader.java
@@ -0,0 +1,15 @@
+package starter;
+
+/*
+ * This class reads a level file and handles the logic behind spawning game objects.
+ */
+
+public class LevelReader {
+	private int level;
+	
+	LevelReader(int levelNum) {
+		level = levelNum;
+	}
+	
+	
+}
