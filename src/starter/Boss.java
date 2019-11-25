1957e3fc77c40f9b75ef73ce403247a514ed507d
ianh <ian.higa@sbcglobal.net>
2019-11-24 16:20:42 -0800

    Implemented boss abilities

--- src/starter/Boss.java
+++ src/starter/Boss.java
@@ -1,27 +1,44 @@
 package starter;
 
 import java.awt.Color;
+import java.util.ArrayList;
 
 import acm.graphics.GImage;
 import acm.graphics.GRect;
+import acm.util.RandomGenerator;
 import javafx.util.Pair;
 
 public class Boss implements Obstacle{
 	
-	private final static double MAX_HEALTH = 500;
+	private final static double MAX_HEALTH = 1000;
 	private double health;
 	private Path flightPath;
 	private Bullet shotType;
 	private GRect sprite;
+	private int curPhase;
+	private GImage target;
+	private double tick;
+	private double fireRate;
+	private int minionRate;
+	private ArrayList<Pair<Enemy, Integer>> minion = new ArrayList<Pair<Enemy, Integer>>();
+	private ArrayList<Pair<Pair<Integer, Path>, Bullet>> phase = new ArrayList<Pair<Pair<Integer, Path>, Bullet>>();
+	private RandomGenerator rand;
 	
-	public Boss(double x, double y, MovementEquation flightType)
+	public Boss(double x, double y, GImage seek)
 	{
 		sprite = new GRect(x,y,300, 50);
 		sprite.setColor(Color.WHITE);
 		sprite.setFillColor(Color.GRAY);
 		sprite.setFilled(true);
-		flightPath = new Path(sprite, flightType, 2, true);
-		health = 500;
+		target = seek;
+		phase.add(new Pair<Pair<Integer, Path>, Bullet>(new Pair<Integer, Path>(1, new Path(sprite, MovementEquation.STAY_SEEK, target)), null));
+		phase.add(new Pair<Pair<Integer, Path>, Bullet>(new Pair<Integer, Path>(0, new Path(sprite, MovementEquation.STAY_SEEK, target)),
+				new WaveBullet(10, sprite, 3, true, true)));
+		minion.add(new Pair<Enemy, Integer>(new Enemy('1', sprite.getX() + sprite.getWidth()/2, target), 20));
+		rand = RandomGenerator.getInstance();
+		curPhase = -1;
+		changePhase();
+		health = MAX_HEALTH;
 	}
 
 	@Override
@@ -41,6 +58,71 @@ public class Boss implements Obstacle{
 		health -= bullet.getDamage();
 		return health;
 	}
+	
+	public Enemy getMinions() {
+		Enemy temp = minion.get(phase.get(curPhase).getKey().getKey() - 1).getKey();
+		if(temp == null) {
+			return temp;
+		}
+		else {
+			double spawnLoc = rand.nextDouble(sprite.getX(), sprite.getX() + sprite.getWidth());
+			temp = new Enemy(temp.getType(), spawnLoc, temp.getTarget());
+			return temp;
+		}
+	}
+	
+	public boolean checkFireRate() {
+		if(tick == fireRate) {
+			tick = 0;
+			return true;
+		}
+		else if(tick > fireRate) {
+			tick = 0;
+			return false;
+		}
+		tick++;
+		return false;
+	}
+	
+	public Bullet getBulletType() {
+		if(shotType instanceof BasicBullet) {
+			BasicBullet temp = new BasicBullet(shotType.getDamage(), shotType.getSprite(), shotType.getSpeed());
+			return temp;
+		}
+		else if(shotType instanceof WaveBullet) {
+			WaveBullet temp = new WaveBullet(shotType.getDamage(), shotType.getSprite(), shotType.getSpeed(), shotType.getDirection(), shotType.checkEnemyBullet());
+			return temp;
+		}
+		return null;
+	}
+	
+	public void changePhase() {
+		curPhase++;
+		if(curPhase == phase.size()) {	
+			curPhase = 0;
+		}
+		flightPath = phase.get(curPhase).getKey().getValue();
+		shotType = phase.get(curPhase).getValue();
+		if(shotType != null) {
+			fireRate = 10;
+		}
+		else {
+			fireRate = 0;
+		}
+		if(phase.get(curPhase).getKey().getKey() != 0) {
+			minionRate = minion.get(phase.get(curPhase).getKey().getKey() - 1).getValue();
+		}
+		else {
+			minionRate = 0;
+		}
+
+	}
+	
+	public int getMinionRate() {
+		
+		return minionRate;
+	}
+	
 	public double getHealthPercentage() {
 		return health/MAX_HEALTH;
 	}
